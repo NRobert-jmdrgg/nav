c73508a050fbbdced26471da678cbb9d855059aa
Róbert Nagy <rob.iskola1@gmail.com>
2022-10-21 11:52:20 +0200

    decrypt token

--- src/operations/tokenExchange.js
+++ src/operations/tokenExchange.js
@@ -1,9 +1,23 @@
-import { userData, softwareData } from '../../config.js';
+import { userData, softwareData, exchangeKey } from '../../config.js';
 import TokenExchangeRequest from '../requests/TokenExchangeRequest.js';
 import sendRequest from '../sendRequest.js';
+import crypto from 'crypto';
 
 export default async function getExchangeToken() {
   const request = new TokenExchangeRequest(...userData, ...softwareData);
   const response = await sendRequest(request, 'tokenExchange');
-  return response['TokenExchangeResponse']['encodedExchangeToken'][0];
+  const encryptedToken =
+    response['TokenExchangeResponse']['encodedExchangeToken'][0];
+
+  // console.log('encrypted: ' + encryptedToken);
+
+  // iv nem kell
+  const decipher = crypto.createDecipheriv('aes-128-ecb', exchangeKey, null);
+
+  // base64 - utf8
+  let exchangeToken = decipher.update(encryptedToken, 'base64', 'utf8');
+  exchangeToken += decipher.final('utf8');
+
+  // console.log('decrypted: ' + exchangeToken);
+  return exchangeToken;
 }

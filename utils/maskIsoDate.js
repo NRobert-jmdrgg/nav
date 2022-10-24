d6a0809f11036dcb0265a707a65b00bbb97454c2
Róbert Nagy <rob.iskola1@gmail.com>
2022-10-24 19:34:55 +0200

    komment + felesleges if statement

--- utils/maskIsoDate.js
+++ utils/maskIsoDate.js
@@ -1,9 +1,11 @@
+/**
+ * ISO dátum átkonvertálása YYYYmmddhhmmss formátumra.
+ * @param {Date} timestamp dátum
+ * @returns {string} YYYYmmddhhmmss formátumú dátum
+ */
 export default function maskIsoDate(timestamp) {
-  if (typeof timestamp === 'string' && timestamp) {
-    const maskedDate =
-      timestamp.substring(0, 10).replaceAll('-', '') +
-      timestamp.substring(11, 19).replaceAll(':', '');
-    return maskedDate;
-  }
-  return null;
+  const maskedDate =
+    timestamp.substring(0, 10).replaceAll('-', '') +
+    timestamp.substring(11, 19).replaceAll(':', '');
+  return maskedDate;
 }

2057a441f1db11505d0a749ef9d3485359eb1cd3
Róbert Nagy <rob.iskola1@gmail.com>
2022-10-24 19:32:54 +0200

    manageInvoice és manageAnnulment requestSignature

--- utils/createRequestSignature.js
+++ utils/createRequestSignature.js
@@ -1,14 +1,34 @@
-import { SHA3 } from 'sha3';
+import crypto from 'crypto';
 import maskIsoDate from '../utils/maskIsoDate.js';
 
+/**
+ * request signature létrehozása
+ * @param {string} requestId kérés idje
+ * @param {Date} timestamp kérés ideje
+ * @param {string} signatureKey technikai felhasználó aláírókulcsa
+ * @param {Array} operations a manageAnnulment vagy manageInvoice operációlistája
+ * @return sha3-512 kódolt request signature
+ */
 export default function createRequestSignature(
   requestId,
   timestamp,
-  signatureKey
+  signatureKey,
+  operations = null
 ) {
-  const hash = SHA3(512);
-  let requestSignature = hash
-    .update(requestId + maskIsoDate(timestamp) + signatureKey)
+  let partialSignature = requestId + maskIsoDate(timestamp) + signatureKey;
+
+  if (operations) {
+    operations.forEach((operation) => {
+      const hash = crypto.createHash('sha3-512');
+      partialSignature += hash
+        .update(operation.operationType + operation.base64data)
+        .digest('hex')
+        .toUpperCase();
+    });
+  }
+  const hash = crypto.createHash('sha3-512');
+  const requestSignature = hash
+    .update(partialSignature)
     .digest('hex')
     .toUpperCase();
 
